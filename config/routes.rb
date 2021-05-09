6c5b15e92b575f769c07ce629db8b51e4478ea15
YusukeMiyao <68069726+s12301800780@users.noreply.github.com>
2021-05-09 22:32:47 +0900

    [update]ルーティングの定義

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      resources :restaurants do
+        resources :foods, only: %i[index]
+      end
+      resources :line_foods, only: %i[index create]
+      #'line_foods/replace'というURLに対してPUTリクエストがきたら、line_foods_controller.rbのreplaceメソッドを呼ぶということを意味する。原則はresources、resourceで書く、例外的に必要であればput ... to:も許容する
+      put 'line_foods/replace', to: 'line_foods#replace'
+      resources :orders, only: %i[create]
+    end
+  end
 end
