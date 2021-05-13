0bddd28e95853c1d16dd209ba0d16e949a040cdc
YusukeMiyao <68069726+s12301800780@users.noreply.github.com>
2021-05-13 23:40:40 +0900

    [add]ルーティングの実装

---
+++ frontend/src/containers/Foods.jsx
@@ -0,0 +1,9 @@
+import React, { Fragment } from "react";
+
+export const Foods = ({ match }) => {
+  return (
+    <Fragment>
+      フード一覧<p>restaurantIdは{match.params.restaurantsId}です</p>
+    </Fragment>
+  );
+};
